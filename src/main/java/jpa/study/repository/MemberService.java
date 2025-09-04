cc889358c314df88a24e2490be8a19d655699367
seo-sang-ho <rocter2049@naver.com>
2025-09-04 21:33:50 +0900

    memberService 코드 작성

---
+++ src/main/java/jpa/study/repository/MemberService.java
@@ -0,0 +1,43 @@
+package jpa.study.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpa.study.domain.Member;
+
+@Service
+@Transactional
+public class MemberService {
+
+	@Autowired // 자동 의존성 주입
+	private MemberRepository memberRepository;
+
+	// 회원 가입
+	public Long join(Member member) {
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		List<Member> findMembers = memberRepository.findByName(member.getName());
+		if (!findMembers.isEmpty()) {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+	}
+
+	// 회원 전체 조회
+	@Transactional(readOnly = true) // 읽기에는 readOnly = true 가 좋다.
+	public List<Member> findAll(){
+		return memberRepository.findAll();
+	}
+
+	// 회원 한명 조회
+	@Transactional(readOnly = true)
+	public Member findOne(Long memberId){
+		return memberRepository.findOne(memberId);
+	}
+}
