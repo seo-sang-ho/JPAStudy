1f69849a871a73ca4a79c708e8721b4310ba3ebe
seo-sang-ho <rocter2049@naver.com>
2025-09-08 20:17:48 +0900

    상품 서비스

---
+++ src/main/java/jpa/study/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpa.study.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpa.study.domain.item.Item;
+import jpa.study.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+	private final ItemRepository itemRepository;
+
+	public void saveItem(Item item) {
+		itemRepository.save(item);
+	}
+
+	public List<Item> findItems(){
+		return itemRepository.findAll();
+	}
+
+	public Item findOne(Long itemId) {
+		return itemRepository.findOne(itemId);
+	}
+}
