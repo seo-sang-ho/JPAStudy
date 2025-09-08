206abb7786ad545425ee6e14f2beb26c7ff57ecb
seo-sang-ho <rocter2049@naver.com>
2025-09-08 20:11:19 +0900

    상품 리포지토리

---
+++ src/main/java/jpa/study/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpa.study.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpa.study.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+	private final EntityManager em;
+
+	public void save(Item item){
+		if(item.getId() == null){
+			em.persist(item);
+		}else{
+			em.merge(item);
+		}
+	}
+
+	public Item findOne(Long id){
+		return em.find(Item.class, id);
+	}
+
+	public List<Item> findAll() {
+		return em.createQuery("select i from item i", Item.class)
+			.getResultList();
+	}
+}
