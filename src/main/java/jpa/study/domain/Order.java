a23a12c35f6f7c53747754141afe71c074289f97
seo-sang-ho <rocter2049@naver.com>
2025-09-09 15:49:11 +0900

    생성로직 변경 제약(생성자 -> PROTECTED)

--- src/main/java/jpa/study/domain/Order.java
+++ src/main/java/jpa/study/domain/Order.java
@@ -16,13 +16,16 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "orders")
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
 	@Id @GeneratedValue
