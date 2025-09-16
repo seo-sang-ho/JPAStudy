2e7fce261eaf630db05ac1e20f09b999ff4cb1b2
seo-sang-ho <rocter2049@naver.com>
2025-09-16 13:34:06 +0900

    상품 등록, 목록, 에러 발생 문제 처리

---
+++ src/main/java/jpa/study/domain/controller/ItemController.java
@@ -0,0 +1,49 @@
+package jpa.study.domain.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jpa.study.domain.item.Book;
+import jpa.study.domain.item.Item;
+import jpa.study.service.ItemService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+	private final ItemService itemService;
+
+	@GetMapping("/items/new")
+	public String createForm(Model model) {
+		model.addAttribute("form", new BookForm());
+		return "items/createItemForm";
+	}
+
+	@PostMapping("/items/new")
+	public String create(BookForm form) {
+
+		Book book = new Book();
+		book.setName(form.getName());
+		book.setPrice(form.getPrice());
+		book.setStockQuantity(form.getStockQuantity());
+		book.setAuthor(form.getAuthor());
+		book.setIsbn(form.getIsbn());
+
+		itemService.saveItem(book);
+		return "redirect:/";
+	}
+
+	@GetMapping("/items")
+	public String list(Model model) {
+		List<Item> items = itemService.findItems();
+		model.addAttribute("items", items);
+		return "items/itemList";
+	}
+}
+
