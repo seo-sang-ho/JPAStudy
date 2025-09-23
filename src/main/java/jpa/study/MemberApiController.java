61dc329b1de43dd24db13f0103b423242d2d66e5
seo-sang-ho <rocter2049@naver.com>
2025-09-23 17:24:39 +0900

    회원 수정 api

--- src/main/java/jpa/study/MemberApiController.java
+++ src/main/java/jpa/study/MemberApiController.java
@@ -3,11 +3,10 @@ package jpa.study;
 import jakarta.validation.Valid;
 import jpa.study.domain.Member;
 import jpa.study.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,6 +28,26 @@ public class MemberApiController {
         Long id = memberService.join(member);
 
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    public static class UpdateMemberRequest {
+        private String name;
     }
 
     @Data
--- src/main/java/jpa/study/service/MemberService.java
+++ src/main/java/jpa/study/service/MemberService.java
@@ -42,4 +42,9 @@ public class MemberService {
 	public Member findOne(Long memberId){
 		return memberRepository.findOne(memberId);
 	}
+
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 }
