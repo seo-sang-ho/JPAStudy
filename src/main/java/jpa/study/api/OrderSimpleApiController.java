80f4354a4a1839ddf27775ece48287b7fdd56fb0
seo-sang-ho <rocter2049@naver.com>
2025-09-29 15:27:31 +0900

    엔티티 직접 노출

--- src/main/java/jpa/study/api/OrderSimpleApiController.java
+++ src/main/java/jpa/study/api/OrderSimpleApiController.java
@@ -1,4 +1,23 @@
 package jpa.study.api;
 
+import jpa.study.domain.Order;
+import jpa.study.repository.OrderRepository;
+import jpa.study.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        return all;
+    }
 }
